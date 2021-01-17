8e4a877e70cf3bddd3fde13a1de74f70dd2757c3
vik <vik>
2021-01-17 13:33:54 +0000

    chore: boilerplate setup
    * webpack
    * babel
    * eslint

---
+++ src/main.js
@@ -0,0 +1,1 @@
+console.log('here we go again');
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.html'
+    })],
+    devtool: 'source-map'
+};
